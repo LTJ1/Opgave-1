f40183043d48bef32d52372ae86d120559d8fcb6
LotteJoergensen <lotte.joergensen@mail.dk>
2017-05-01 15:22:50 +0200

    Start

--- Opgave 1/Program.cs
+++ Opgave 1/Program.cs
@@ -10,6 +10,20 @@ namespace Opgave_1
     {
         static void Main(string[] args)
         {
+            // Udskriv tekst til skærm
+            Console.WriteLine("Indtasat navn:");
+            string Navn;
+            
+            // Læs tekst fra skærm
+            Navn = Console.ReadLine();
+            string StortNavn;
+            // Konverter navn til store bogstaver
+            StortNavn = Navn.ToUpper();
+
+            // Udskriv navn med store bogstaver 
+            Console.WriteLine("Velkommen " + StortNavn);
+            Console.WriteLine("Tryk en tast for at fortsætte");
+            Console.ReadKey();
         }
     }
 }
