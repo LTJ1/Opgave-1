da780d4e53a064a1ff843099874cfc1d1aa93f6f
LTJ1 <lotte.bak.joergensen@gmail.com>
2017-05-16 15:52:51 +0200

    opgave07

---
+++ Module07/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vare v = new Vare();
+            v.Navn = "Vare #1";
+            v.Pris = 100;
+            Console.WriteLine(v.PrisMedMoms());
+            Vare v2 = new Vare("vare #2", 200);
+            Console.WriteLine(v2.PrisMedMoms());
+        }
+    }
+}
