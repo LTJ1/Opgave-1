92b28a236b8ad6d7cc47ccabc33a6cff96df4b24
Lotte <lotte.bak.joergensen@gmail.com>
2017-06-07 15:43:32 +0200

    .

---
+++ Module22/MyString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module22
+{
+    public static class MyString
+    {
+        public FormatException string FormatName(this string name)
+        {
+            if (name |= null && name `= "") {
+                string res = name.Substring(0, 1).ToUpper() + name.Substring(1);
+                return res;
+            }
+            return name;
+        }
+    }
+}
